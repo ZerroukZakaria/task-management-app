f16060769cc360079606129afcd97dfd8b807291
Zeke Zerk <zerrouk.zakaria2002@gmail.com>
2023-12-18 07:37:41 +0100

    Update CORS settings

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://fabulous-bavarois-41eb57.netlify.app/'],
 
     'allowed_origins_patterns' => [],
 
